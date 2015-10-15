de5d8ce2dc354476f2bc21a34307ea0579a45c1b
Edward-Zam <edward.zamora@outlook.com>
2015-10-15 15:24:32 -0700

    Initial upload of transpose matrix example
    Contains 1024 x 1024 matrix instantiation with serial and parallel cuda code.

---
+++ TransposeMatEx/GoldenCode.cu
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include "gputimer.h"
+
+const int N = 1024;
+
+void transpose_CPU(float in [], float out[])
+{
+	for(int j=0;j<N;j++)
+		for(int i=0;i<N;i++)
+			out[j + i*N] = in[i + j*N]; //out(j,i) = in(i,j)
+}
+
+__global void 
+transpose_serial(float in [], float out[])
+{
+	for(int j=0;j<N;j++)
+		for(int i=0;i<N;i++)
+			out[j + i*N] = in[i + j*N]; //out(j,i) = in(i,j)
+}
+
+__global__ void
+transpose_parallel_per_row(float in[], float out[])
+{
+	int i = threadIdx.x;
+	
+	for(int j=0; j<N;j++)
+		out[i + i*N] = in[i + j*N];
+}
+
+int main(int argc, char **argv)
+{
+	int numbytes = N*N*sizeof(float);
+	
+	float *in	= (float *) malloc(numbytes);
+	float *out	= (float *) malloc(numbytes);
+	float *gold	= (float *) malloc(numbytes);
+	
+	fill_matrix(in);
+	transpose_CPU(in, gold);
+	
+	float *d_in, *d_out;
+	
+	cudaMalloc(&d_in, numbytes);
+	cudaMalloc(&d_out, numbytes);
+	cudaMemcpy(d_in, in, numbytes, cudaMemcpyHostToDevice);
+	
+	GpuTimer timer;
+	timer.Start();
+	transpose_serial<<<1,1>>>(d_in, d_out); //takes about 0.084768 ms for N = 8; 
+	// transpose_serial: 466.429 ms for N = 1024;
+	timer.Stop();
+	cudaMemcpy(out, d_out, numbytes, cudaMemcpyDeviceToHost);
+	printf("transpose_serial: %g ms. \nVerifying transpose...%s\n,", 
+		timer.Elapsed(),
+		compare_matrices(out,gold) ? "Failed": "Success"); 
+		
+	timer.Start();	
+	transpose_parallel_per_row<<<1,N>>>(d_in, d_out);
+	// transpose_parallel_per_row: 4.76848 ms
+	timer.Stop();
+	cudaMemcpy(out, d_out, numbytes, cudaMemcpyDeviceToHost);
+	printf("transpose_parallel_per_row: %g ms. \nVerifying transpose...%s\n,", 
+		timer.Elapsed(),
+		compare_matrices(out,gold) ? "Failed": "Success"); 
+	
+//	printf("input matrix:\n"); print_matrix(in);
+//	printf("reference or 'gold' transposed matrix:\n"); print_matrix(gold);
+	
+	
+}
